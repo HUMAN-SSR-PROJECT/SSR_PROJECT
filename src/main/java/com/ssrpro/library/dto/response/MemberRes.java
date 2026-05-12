1b37235ae1304d9007b2eff906662bb6445554c8
admin <min011128g@gmail.com>
2026-05-12 16:39:05 +0900

    20260512-박민 member 엔티티, 리스폰스 수정

--- src/main/java/com/ssrpro/library/dto/response/MemberRes.java
+++ src/main/java/com/ssrpro/library/dto/response/MemberRes.java
@@ -1,4 +1,45 @@
 package com.ssrpro.library.dto.response;
 
+import com.ssrpro.library.dto.entity.Members;
+import lombok.*;
+import org.springframework.boot.json.JsonWriter;
+
+import java.lang.reflect.Member;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
 public class MemberRes {
+    private Long id;
+    private String nickname;
+    private String email;
+    private String name;
+    private LocalDate birth;
+    private String rule;
+    private String imgUrl;
+    private String addr;
+    private String intro;
+    private String state;
+    private LocalDateTime createdAt;
+
+    public static MemberRes from(Members members) {
+        return MemberRes.builder()
+                .id(members.getId())
+                .nickname(members.getNickname())
+                .email(members.getEmail())
+                .name(members.getName())
+                .birth(members.getBirth())
+                .rule(members.getRule())
+                .imgUrl(members.getImgUrl())
+                .addr(members.getAddr())
+                .intro(members.getIntro())
+                .state(members.getState())
+                .createdAt(members.getCreatedAt())
+                .build();
+    }
 }
