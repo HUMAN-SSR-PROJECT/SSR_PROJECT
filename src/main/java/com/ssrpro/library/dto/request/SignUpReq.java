85bbb48219d65da06e11a255936a6dfe9fdc246e
admin <min011128g@gmail.com>
2026-05-13 12:16:38 +0900

    20260513-박민/유저엔티티,Res 수정 및 로그인 회원가입 Req 작업

--- src/main/java/com/ssrpro/library/dto/request/SignUpReq.java
+++ src/main/java/com/ssrpro/library/dto/request/SignUpReq.java
@@ -1,4 +1,32 @@
 package com.ssrpro.library.dto.request;
 
+import com.ssrpro.library.dto.entity.Members;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class SignUpReq {
+    private String email;
+    private String name;
+    private String nickname;
+    private LocalDate birth;
+    private String password;
+
+    public Members toEntity() {
+        return Members.builder()
+                .email(this.email)
+                .name(this.name)
+                .nickname(this.nickname)
+                .birth(this.birth)
+                .password(this.password)
+                .state("활동")
+                .rule("N")
+                .build();
+    }
 }
