1e0279663df9638fe503a91bfea2cca84d8529ca
admin <misun0714@naver.com>
2026-05-13 12:14:18 +0900

    20260513-장미선-Request(도서검색, 아이디 찾기)

--- src/main/java/com/ssrpro/library/dto/request/FindIdReq.java
+++ src/main/java/com/ssrpro/library/dto/request/FindIdReq.java
@@ -1,4 +1,25 @@
 package com.ssrpro.library.dto.request;
 
+
+import com.ssrpro.library.dto.entity.Members;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class FindIdReq {
+    private String name; // 실명
+    private LocalDate birth; // 생년월일
+
+    public Members toEntity() {
+        return Members.builder()
+                .name(this.name)
+                .birth(this.birth)
+                .build();
+    }
 }
