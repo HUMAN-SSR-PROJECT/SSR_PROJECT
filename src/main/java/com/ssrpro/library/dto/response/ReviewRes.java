991938d2594b67e49da2e5e2cfc66da8fc7a19a9
dotdit99 <dotdit99@icloud.com>
2026-05-13 09:38:17 +0900

    20260513_강성민/ 컨펌받은 내용 수정

--- src/main/java/com/ssrpro/library/dto/response/ReviewRes.java
+++ src/main/java/com/ssrpro/library/dto/response/ReviewRes.java
@@ -13,29 +13,12 @@ import java.time.LocalDateTime;
 @Valid
 public class ReviewRes {
 
-  private Long reviewId;
   private String reviewComment;
   private Double reviewRating;
   private LocalDateTime reviewCreatedAt;
   private LocalDateTime reviewUpdatedAt;
 
-  private Long bookId;
-  private String bookTitle;
-  private String bookAuthor;
-  private String bookPublisher;
-  private String bookDescription;
-  private String bookImageUrl;
-
-  private Long memberId;
-  private String memberName;
-  private String memberNickname;
-  private String memberEmail;
-  private String memberProfileImage;
-  private String memberRole;
-
   private Long likeCount;
   private Boolean isLiked;
   private Long commentCount;
-  private Double bookAverageRating;
-  private Long bookTotalReviews;
 }
