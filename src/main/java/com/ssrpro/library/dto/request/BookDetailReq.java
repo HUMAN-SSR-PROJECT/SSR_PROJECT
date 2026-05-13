573711376d79e683194bebad2083851074010133
admin <misun0714@naver.com>
2026-05-13 15:55:56 +0900

    20260513-장미선-request에서 BookSearchReq를 BookDetailReq로 변경, 내용은 keyword를 bookId로 변경

--- src/main/java/com/ssrpro/library/dto/request/BookDetailReq.java
+++ src/main/java/com/ssrpro/library/dto/request/BookDetailReq.java
@@ -1,7 +1,6 @@
 package com.ssrpro.library.dto.request;
 
 
-import com.ssrpro.library.dto.entity.Book;
 import lombok.*;
 
 @Getter
@@ -10,8 +9,8 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class BookSearchReq {
-    private String keyword; // 검색 키워드
+public class BookDetailReq {
+    private String bookId;
     private String city; // 시.도
     private String district; //구.군
 
