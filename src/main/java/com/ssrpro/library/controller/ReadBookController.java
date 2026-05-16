a3662afcf5c7152150d855fe38b23a4a95a2658f
JunHee <cyanaria89@gmail.com>
2026-05-16 13:25:45 +0900

    20260516-홍준희/ReadBook-Controller, Service 수정

--- src/main/java/com/ssrpro/library/controller/ReadBookController.java
+++ src/main/java/com/ssrpro/library/controller/ReadBookController.java
@@ -1,18 +1,21 @@
 package com.ssrpro.library.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ssrpro.library.dto.request.ReadBookReq;
+import com.ssrpro.library.dto.security.CustomUser;
 import com.ssrpro.library.service.ReadBookService;
 
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequestMapping("/mylib")
@@ -21,7 +24,13 @@ public class ReadBookController {
   private final ReadBookService readBookService;
 
   @GetMapping("/readbook")
-  public String readBook(Model model, @RequestParam(required = false) Long bookId) {
+  public String readBook(
+    Model model,
+    @AuthenticationPrincipal CustomUser customUser, 
+    @RequestParam(required = false) Long bookId
+  ) {
+    Long memberId = customUser.getMemberId();
+
     // 내 서재 - 읽을 책 조회
     model.addAttribute("readSoonList", readBookService.readSoonList(memberId));
 
@@ -41,28 +50,46 @@ public class ReadBookController {
 
   // 읽을 책 추가 / 삭제
   @PostMapping("/readbook/{bookId}/soon")
-  public String addToReadSoon(@PathVariable Long bookId) {
+  public String addToReadSoon(
+    @AuthenticationPrincipal CustomUser customUser, 
+    @PathVariable Long bookId
+  ) {
+    Long memberId = customUser.getMemberId();
+
     readBookService.addToReadSoon(memberId, bookId);
     return "redirect:/mylib/readbook";
   }
 
   // 내 서재 - 읽는 중 추가 / 삭제
   @PostMapping("/readbook/{bookId}/reading")
-  public String addToReading(@PathVariable Long bookId) {
+  public String addToReading(
+    @AuthenticationPrincipal CustomUser customUser, 
+    @PathVariable Long bookId
+  ) {
+    Long memberId = customUser.getMemberId();
+
     readBookService.addToReading(memberId, bookId);
     return "redirect:/mylib/readbook";
   }
   
   // 내 서재 - 읽는 중 → 완독, 완독 상세 수정
-  @PostMapping("/readbook/{bookId}/readed")
-  public String changeToReaded(@PathVariable Long bookId) {
-    readBookService.changeToReaded(memberId, bookId);
-    return "redirect:/mylib/readbook";
+  @PostMapping("/readbook/readed")
+  public String changeToReaded(
+    @AuthenticationPrincipal CustomUser customUser, 
+    @RequestParam Long bookId,
+    @ModelAttribute ReadBookReq req
+  ) {
+    Long memberId = customUser.getMemberId();
+    req.setBookId(bookId);
+
+    readBookService.changeToReaded(memberId, req);
+    return "redirect:/mylib/readbook?bookId=" + bookId;
   }
   
   // 독서 분석 리포트 - 통계 계산용 리스트
   @GetMapping("/stats")
-  public String rawDate(Model model) {
+  public String rawDate(Model model, @AuthenticationPrincipal CustomUser customUser) {
+    Long memberId = customUser.getMemberId();
     model.addAttribute("rawDate", readBookService.getRawData(memberId));
     return "mylib/stats";
   }
